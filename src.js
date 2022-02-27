4dafeda5498c6f0566334588be39ca655426bdaf
An algorithm must be seen to be believed." Donald Knuth <Alfred.nehemie@gmail.com>
2022-02-27 18:36:50 +0200

    Create src.js

---
+++ src.js
@@ -0,0 +1,70 @@
+const utils = {
+    numberToReadable(number) => {
+        /*
+        Returns a human-readable string of a given number
+        */
+        let formated_number = NaN
+        if (number !== "") {
+            try {
+                if (typeof (number) != "number") number = parseFloat(number)
+                formated_number = (Math.round((number + Number.EPSILON) * 1000) / 1000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            }
+            catch (e) {
+                console.log("Failed to format number.");
+            }
+        }
+        else {
+            formated_number = ""
+        }
+        return formated_number
+    },
+      
+  compareObjects = function (obj1, obj2) {
+      /*
+        Compares 2 objects for equality
+        From: https://gist.github.com/nicbell/6081098"
+      */
+    
+      //Loop through properties in object 1
+      for (var p in obj1) {
+          //Check property exists on both objects
+          if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
+
+          switch (typeof (obj1[p])) {
+              //Deep compare objects
+              case 'object':
+                  if (!sessioner.compare(obj1[p], obj2[p])) return false;
+                  break;
+              //Compare function code
+              case 'function':
+                  if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) return false;
+                  break;
+              //Compare values
+              default:
+                  if (obj1[p] != obj2[p]) return false;
+          }
+      }
+
+      //Check object 2 for any extra properties
+      for (var p in obj2) {
+          if (typeof (obj2[p]) == 'undefined') return false;
+      }
+      return true;
+  },
+    formToJSON: form_name => {
+      /*
+        Returns a JS object built from a form frm_name using name attributes
+      */
+    let formData = new FormData(document.querySelector(`[name=${form_name}]`))
+    let jsonData = {}
+    formData.forEach((value, key) => {
+        if (!isNaN(value)) {
+            if (value.indexOf(".")) value = parseFloat(value)
+            else { value = parseInt(value) }
+        }
+        jsonData[key] = value
+    })
+    return jsonData
+}
+
+}
