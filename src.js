8ab5c549681b6ede7d9ad7b85e8ea1205efa0a63
An algorithm must be seen to be believed." Donald Knuth <Alfred.nehemie@gmail.com>
2022-02-27 18:39:21 +0200

    clean up

--- src.js
+++ src.js
@@ -1,5 +1,5 @@
 const utils = {
-    numberToReadable(number) => {
+    numberToReadable: function (number) {
         /*
         Returns a human-readable string of a given number
         */
@@ -18,53 +18,54 @@ const utils = {
         }
         return formated_number
     },
-      
-  compareObjects = function (obj1, obj2) {
-      /*
-        Compares 2 objects for equality
-        From: https://gist.github.com/nicbell/6081098"
-      */
-    
-      //Loop through properties in object 1
-      for (var p in obj1) {
-          //Check property exists on both objects
-          if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
 
-          switch (typeof (obj1[p])) {
-              //Deep compare objects
-              case 'object':
-                  if (!sessioner.compare(obj1[p], obj2[p])) return false;
-                  break;
-              //Compare function code
-              case 'function':
-                  if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) return false;
-                  break;
-              //Compare values
-              default:
-                  if (obj1[p] != obj2[p]) return false;
-          }
-      }
+    compareObjects: function (obj1, obj2) {
+        /*
+          Compares 2 objects for equality
+          From: https://gist.github.com/nicbell/6081098"
+        */
 
-      //Check object 2 for any extra properties
-      for (var p in obj2) {
-          if (typeof (obj2[p]) == 'undefined') return false;
-      }
-      return true;
-  },
-    formToJSON: form_name => {
-      /*
-        Returns a JS object built from a form frm_name using name attributes
-      */
-    let formData = new FormData(document.querySelector(`[name=${form_name}]`))
-    let jsonData = {}
-    formData.forEach((value, key) => {
-        if (!isNaN(value)) {
-            if (value.indexOf(".")) value = parseFloat(value)
-            else { value = parseInt(value) }
+        //Loop through properties in object 1
+        for (var p in obj1) {
+            //Check property exists on both objects
+            if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
+
+            switch (typeof (obj1[p])) {
+                //Deep compare objects
+                case 'object':
+                    if (!sessioner.compare(obj1[p], obj2[p])) return false;
+                    break;
+                //Compare function code
+                case 'function':
+                    if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) return false;
+                    break;
+                //Compare values
+                default:
+                    if (obj1[p] != obj2[p]) return false;
+            }
         }
-        jsonData[key] = value
-    })
-    return jsonData
-}
+
+        //Check object 2 for any extra properties
+        for (var p in obj2) {
+            if (typeof (obj2[p]) == 'undefined') return false;
+        }
+        return true;
+    },
+    
+    formToJSON: function (form_name) {
+        /*
+          Returns a JS object built from a form frm_name using name attributes
+        */
+        let formData = new FormData(document.querySelector(`[name=${form_name}]`))
+        let jsonData = {}
+        formData.forEach((value, key) => {
+            if (!isNaN(value)) {
+                if (value.indexOf(".")) value = parseFloat(value)
+                else { value = parseInt(value) }
+            }
+            jsonData[key] = value
+        })
+        return jsonData
+    }
 
 }
