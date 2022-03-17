d48d4d38f16b78231ae06170d6be2d6c8e048312
An algorithm must be seen to be believed." Donald Knuth <Alfred.nehemie@gmail.com>
2022-03-17 07:28:17 +0200

    updated to consider checkbox value, on to boolean true

--- src.js
+++ src.js
@@ -61,6 +61,7 @@ const utils = {
         formData.forEach((value, key) => {
             if (!isNaN(value)) {
                 if (value.indexOf(".")) value = parseFloat(value)
+                if (value='on') value = true
                 else { value = parseInt(value) }
             }
             jsonData[key] = value
